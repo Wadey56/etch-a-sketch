b2405bb9c9764278bf233ebc27cfb27abf8421b2
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-07-23 19:42:09 +0100

    Add mouseover event listener to 16x16 div grid

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let container = document.querySelector("#container");
 
+// create 16 div rows
 for (let i = 0; i < 16; i++) {
     let row = document.createElement("div");
-    row.className = "row"
+    row.className = "row";
+
+    // create 16 div elements in each row
     for (let j = 0; j < 16; j++ ) {
         let square = document.createElement("div");
+        square.className = "square";
         row.appendChild(square);
+
+        // event listener for mouse over 
+        square.addEventListener("mouseenter", () => {
+            console.log("Works")
+        })
     }
     container.appendChild(row);
 }
